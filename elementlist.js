4f41c4a91f2b26f7466f2a701b25a80bd76681aa
Zemistr <zemistr@gmail.com>
2016-01-10 10:58:54 +0100

    Added documentation

---
+++ elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","Yep\\Stopwatch\\Event"],["c","Yep\\Stopwatch\\Formatter"],["c","Yep\\Stopwatch\\Lap"],["c","Yep\\Stopwatch\\Manager"],["c","Yep\\Stopwatch\\Stopwatch"],["c","Yep\\Stopwatch\\StopwatchAlreadyExistsException"],["c","Yep\\Stopwatch\\StopwatchDoesntExistException"],["c","Yep\\Stopwatch\\StopwatchEventDoesntExistException"],["c","Yep\\Stopwatch\\StopwatchLapNotStartedException"]];
